5eb3edf33d07a459ffc76d6102ae0db5a182f632
VuongDuongg <funnyvuong@gmail.com>
2026-05-12 07:37:09 +0700

    commit
    commit

--- Adapter/Shape.java
+++ Adapter/Shape.java
@@ -1,7 +1,16 @@
 package Adapter;
 
 public class Shape {
-    
+    public static void main(String[] args) {
+        RoundHole hole = new RoundHole(5);
+        RoundPeg roundPeg = new RoundPeg(5);
+        SquarePeg squarePeg = new SquarePeg(5);
+
+        System.out.println("Round peg fits round hole: " + hole.fits(roundPeg));
+
+        SquarePegAdapter squarePegAdapter = new SquarePegAdapter(squarePeg);
+        System.out.println("Square peg fits round hole: " + hole.fits(squarePegAdapter));
+    }
 }
 
 class RoundHole {
@@ -15,7 +24,7 @@ class RoundHole {
         return radius;
     }
 
-    public boolean fitPeg(RoundPeg peg) {
+    public boolean fits(RoundPeg peg) {
         return this.getRadius() >= peg.getRadius();
     }
 }
@@ -32,4 +41,23 @@ class RoundPeg {
     }
 }
 
+class SquarePeg {
+    private double width;
 
+    public SquarePeg(double width) {
+        this.width = width;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+}
+
+class SquarePegAdapter extends RoundPeg {
+    private SquarePeg peg;
+
+    public SquarePegAdapter(SquarePeg peg) {
+        super(peg.getWidth() * Math.sqrt(2) / 2);
+        this.peg = peg;
+    }
+}
